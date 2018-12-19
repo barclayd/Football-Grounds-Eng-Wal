4ef4f5994ca4a3db2a74ecbd99210d2f62bbbbc5
Daniel Barclay <barclaysd@me.com>
2018-12-19 17:25:35 +0000

    Added clusters and refactored code

--- football-grounds.py
+++ football-grounds.py
@@ -1,7 +1,8 @@
 import folium
+from folium.plugins import MarkerCluster
 import pandas as pd
 
-# Load Data
+# Load data from .txt file
 data = pd.read_csv("football-data.txt")
 lat = data['Latitude']
 lon = data['Longitude']
@@ -10,12 +11,12 @@ city = data['City']
 country = data['Country']
 
 # base map
-map = folium.Map(location=[51.481583, -3.179090], zoom_start=5, tiles='CartoDB dark_matter')
+grounds_map = folium.Map(location=[51.481583, -3.179090], zoom_start=5, tiles='CartoDB dark_matter')
 
 # change colour based on country
 
 
-def colour_change(country):
+def colour_change(location):
 
     switcher = {
         'England': 'green',
@@ -24,20 +25,19 @@ def colour_change(country):
         'Scotland': 'black',
         'Spain': 'red'
     }
-    return switcher.get(country, lambda: 'gray')
+    return switcher.get(location, lambda: 'gray')
 
 
 # first ground
-folium.Marker(location=[51.4728, -3.2030], popup="Cardiff City Stadium", icon=folium.Icon(color='blue')).add_to(map)
+folium.CircleMarker(location=[53.3831, -2.3356], popup="Altrincham FC, Altrincham", color='red', opacity='0.8', radius=5).add_to(grounds_map)
 
-# multiple grounds in Swansea
-for coordinates in [[51.6427, -3.9346], [51.6834, -4.1478], [51.596249, -3.780910]]:
-    folium.Marker(location=coordinates, icon=folium.Icon(color='gray')).add_to(map)
+# create cluster of grounds
+cluster = MarkerCluster().add_to(grounds_map)
 
 # plot grounds
 for lat, lon, team, country, city in zip(lat, lon, team, country, city):
     folium.CircleMarker(location=[lat, lon], popup=str(team) + " FC" + ", " + str(city),
-                        radius=9, fill_opacity=0.9,
-                        fill_color=(colour_change(country))).add_to(map)
+                        radius=9, fill_opacity=0.8, color='blue',
+                        fill_color=(colour_change(country))).add_to(cluster)
 
-map.save("football-grounds.html")
+grounds_map.save("football-grounds.html")
